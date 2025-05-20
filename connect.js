1b9c93af3c7673fd94de4264700cb57d3a7e67e3
Krishna Sharma <155765885+Krishna4298@users.noreply.github.com>
2025-05-20 23:45:30 +0530

    Add files via upload

---
+++ connect.js
@@ -0,0 +1,13 @@
+const mongooes = require("mongoose");
+
+uri = "mongodb+srv://studyplanner:bRUpb2N6FhkeCEew@studyplanner.8sl7ytp.mongodb.net/Studyplanner?retryWrites=true&w=majority&appName=Studyplanner";
+
+
+const connectDB = () => {
+    return mongooes.connect(uri, {
+        userNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+};
+
+module.exports = connectDB;
